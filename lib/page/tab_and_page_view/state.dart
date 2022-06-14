41e7388def10f4edfc34cd7943218c31a6f33cb4
10457 <yx891167~>
2022-06-14 17:57:36 +0800

    新增部分页面

---
+++ lib/page/tab_and_page_view/state.dart
@@ -0,0 +1,5 @@
+class TabAndPageViewState {
+  TabAndPageViewState() {
+    ///Initialize variables
+  }
+}
---
+++ lib/page/tab_and_page_view/view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'logic.dart';
+
+class TabAndPageViewPage extends StatelessWidget {
+  final logic = Get.put(TabAndPageViewLogic());
+  final state = Get.find<TabAndPageViewLogic>().state;
+
+  TabAndPageViewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(toString()),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('功能待开发'),
+      ),
+    );
+  }
+}
